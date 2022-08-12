ad6518f736b0eb55e0df6404ae988ac143d27d02
sonnka <sonyakazanceva1331@gmail.com>
2022-08-12 17:08:33 +0300

    Add Main.java, update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,6 +2,10 @@ public class User {
     private String name;
     private String surname;
 
+    public User(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
     @Override
     public String toString(){
         return "User: " + this.name + " " + this.surname;
