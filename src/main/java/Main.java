1a9953085fd4b5157c7062eb23dcd3a15a688f9b
sonnka <sonyakazanceva1331@gmail.com>
2022-08-12 17:14:27 +0300

    Add string Age for User

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        User user1 = new User("John","Doe");
+        User user1 = new User("John","Doe",28);
         user1.toString();
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,10 +1,12 @@
 public class User {
     private String name;
     private String surname;
+    private Integer age;
 
-    public User(String name, String surname){
+    public User(String name, String surname, int age){
         this.name = name;
         this.surname = surname;
+        this.age = age;
     }
     @Override
     public String toString(){
