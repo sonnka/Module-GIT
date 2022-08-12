7b20440062ceb64c36ddb3f70b9d8cee0265119e
sonnka <sonyakazanceva1331@gmail.com>
2022-08-12 17:03:51 +0300

    Update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,4 +1,9 @@
 public class User {
     private String name;
     private String surname;
+
+    @Override
+    public String toString(){
+        return "User: " + this.name + " " + this.surname;
+    }
 }
